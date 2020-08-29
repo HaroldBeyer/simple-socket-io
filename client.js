968a0384a7eadb5d6e21be836c28f4e0877afd19
Haroldo <haroldobeyerbacellar@gmail.com>
2020-08-29 19:08:30 -0300

    Update with tcp server

--- client.js
+++ client.js
@@ -1,7 +1,10 @@
 const client = require('socket.io-client')
-const socket = client.connect('http://localhost:8081')
+const socket = client.connect('http://localhost:8081');
+
+// const socket = client.connect('http://localhost:3000')
 
 socket.on('connect', () => {
+    console.log("Connected");
     socket.on('time', (time) => {
         console.log('Time: ', time);
     });
--- server.js
+++ server.js
@@ -1,22 +1,56 @@
+const app = require('http').createServer(handler),
+    io = require('socket.io').listen(app),
+    fs = require('fs');
 
-'use strict';
-
-const app = require('express')();
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
-
-const PORT = process.env.PORT || 8081;
-
-http.listen(PORT, function () {
-    console.log('Servidor rodando em: http://localhost:8081');
+app.listen(8081, () => {
+    console.log('Servidor socket io na porta 8081');
 });
 
-io.on('connection', (socket) => {
-    console.log('Client connected');
-    socket.on('disconnect', () => console.log('Client disconnected'));
-    socket.on('ola', data => {
-        console.log('ola', data);
+function handler(req, res) {
+    fs.readFile(__dirname + '/index.html', function (err, data) {
+        if (err) {
+            res.writeHead(500);
+            return res.end('Error');
+        }
+        res.writeHead(200);
+        res.write(data);
+        res.end();
+    })
+};
+
+io.sockets.on('connection', (socket) => {
+    console.log('connection...');
+    socket.on('emit_from_client', (data) => {
+        console.log('socket.io server recebe : ' + data);
+        io.sockets.emit('emit_from_server', data);
     });
+
+    socket.on('ola', (data) => {
+        console.log('Ola: ', data);
+    });
+
+    setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
 });
 
-setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
+const net = require('net');
+
+net.createServer((socket) => {
+    console.log('socket conectado');
+    socket.on('data', function (data) {
+        const line = data.toString();
+        console.log('got "data"', line);
+        io.sockets.emit('emit_from_server', line);
+    });
+    socket.on('end', function () {
+        console.log('end');
+    });
+    socket.on('close', function () {
+        console.log('close');
+    });
+    socket.on('error', function (e) {
+        console.log('error ', e);
+    });
+    socket.write('ola tcp server');
+}).listen(3080, function () {
+    console.log('TCP Server na porta 3080');
+});
